b5908b26823f7fc725dcb344eed2a6f2cc94cc34
FranciscoTapia <frantapia01@gmail.com>
2026-09-16 23:09:20 -0300

    agrego logica de conexion y send para queue

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -1,12 +1,45 @@
 import pika
-import random
-import string
-from .middleware import MessageMiddlewareQueue, MessageMiddlewareExchange
+from pika.exceptions import AMQPConnectionError, StreamLostError, ConnectionClosedByBroker
+from .middleware import (
+    MessageMiddlewareQueue,
+    MessageMiddlewareExchange,
+    MessageMiddlewareMessageError,
+    MessageMiddlewareDisconnectedError,
+    MessageMiddlewareCloseError,
+    MessageMiddlewareDeleteError,
+)
 
 class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
-
     def __init__(self, host, queue_name):
-        pass
+        self._host = host
+        self._queue_name = queue_name
+        self._consumer_tag = None
+        self._is_consuming = False
+
+        try:
+            self._connection = pika.BlockingConnection(pika.ConnectionParameters(host=self._host))
+            self._channel = self._connection.channel()
+            self._channel.queue_declare(queue=self._queue_name, durable=False)
+
+        except (AMQPConnectionError, StreamLostError) as e:
+            raise MessageMiddlewareDisconnectedError(f"Error de conexion: {e}") from e
+        except Exception as e:
+            raise MessageMiddlewareMessageError(f"Error inicializando la cola: {e}") from e
+        
+    
+    def send(self, message):
+        try:
+            self._channel.basic_publish(
+                exchange='',
+                routing_key=self._queue_name,
+                body=message
+            )
+        except (AMQPConnectionError, StreamLostError, ConnectionClosedByBroker) as e:
+            raise MessageMiddlewareDisconnectedError(f"Conexion perdida al enviar: {e}") from e
+        except Exception as e:
+            raise MessageMiddlewareMessageError(f"Error al enviar mensaje: {e}") from e
+
+
 
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     
